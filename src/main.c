43998979af1716ed53c8e9a71da51face9b7b15b
Andrew Hong <35881688+novialriptide@users.noreply.github.com>
2022-06-28 02:34:07 -0400

    Added complete word select

--- src/main.c
+++ src/main.c
@@ -1,17 +1,42 @@
 #include <gb/gb.h>
+#include <types.h>
 #include <stdint.h>
+#include <stdio.h>
 
 
-void main(void)
+void main()
 {
-    // Loop forever
+    uint8_t joydata;
+    int selected_letter_index = 65; // 65 to 90
+    int word_index = 0;
+    char player_word[6];
     while(1) {
+        printf("Word: %s\n", player_word);
+        printf("Selected Letter: %c\n", selected_letter_index);
+        
+        while(1) {
+            joydata = joypad();
+            if (joydata & J_RIGHT) {
+                if (selected_letter_index + 1 <= 90) {
+                    selected_letter_index++;
+                    break;
+                }
+            }
+            if (joydata & J_LEFT) {
+                if (selected_letter_index - 1 >= 65) {
+                    selected_letter_index--;
+                    break;
+                }
+            }
+            if (joydata & J_A && word_index < 5) {
+                player_word[word_index] = (char) selected_letter_index;
+                word_index++;
+                break;
+            }
+        }
+        cls();
+        delay(200);
 
-
-		// Game main loop processing goes here
-
-
-		// Done processing, yield CPU and wait for start of next frame
         wait_vbl_done();
     }
 }
