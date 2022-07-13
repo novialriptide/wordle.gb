04e0024a49f408ff4f99aa9506987e30a6cf9911
Andrew Hong <andrewhong@myyahoo.com>
2022-07-13 02:20:57 -0400

    More words

--- src/main.c
+++ src/main.c
@@ -7,8 +7,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <rand.h>
-#include "main.h"
-#include "tiles.c"
+#include "words.c"
 
 #define GAMEBOY_TILE_LENGTH 8
 
@@ -36,10 +35,6 @@ void draw_word(char text[], int length, int x, int y) {
 }
 
 void main() {
-    // Load tiles
-    set_sprite_data(0, 26, tiles);
-
-
     uint16_t seed = DIV_REG;
     gotogxy(3, 6);
     gprintf("Wordle by");
@@ -57,20 +52,8 @@ void main() {
     seed |= (uint16_t)DIV_REG << 8;
     initrand(seed);
 
-    const char *WORD_LIST[WORD_LIST_LENGTH];
-    WORD_LIST[0] = "seize";
-    WORD_LIST[1] = "serve";
-    WORD_LIST[2] = "vader";
-    WORD_LIST[3] = "buzzy";
-    WORD_LIST[4] = "weird";
-    WORD_LIST[5] = "prime";
-    WORD_LIST[6] = "light";
-    WORD_LIST[7] = "piano";
-    WORD_LIST[8] = "water";
-    WORD_LIST[9] = "shark";
-
     uint8_t joydata;
-    uint8_t word_choice_index = rand() % WORD_LIST_LENGTH;
+    uint8_t word_choice_index = rand() % sizeof(WORD_LIST) / sizeof(WORD_LIST[0]);
     int chances = 5;
     int selected_letter_index = 65; // 65 to 90
     int word_index = 0;
